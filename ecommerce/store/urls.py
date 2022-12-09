4d00dc1b2e9c5f57dbc903eabf0953a6e8399cb9
Arafathislam <islamarafath315@gmail.com>
2022-12-10 00:04:09 +0600

    working

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('',views.store,name="store"),
     path('cart/',views.cart,name="cart"),
     path('checkout/',views.checkout,name="checkout"),
+    path('update_item/',views.updateItem, name="update_item"),
+    path('process_order/',views.processOrder, name="porcess_order")
 
 ]
